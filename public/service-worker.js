561f257613fe5ff481d81c8ce88c3d98a0e79d26
jin <2441566215@qq.com>
2024-04-05 11:55:56 +0100

    3

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
 self.addEventListener("install", (event) => {
     console.log("Service worker installing...");
-    // 这里，你将稍后添加用于缓存资源的代码。
-});
+    // Here, you'll later add code for actions like caching assets.
+    });

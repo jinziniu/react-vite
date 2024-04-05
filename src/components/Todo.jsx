45d216a71cde52b2e1ed75279b34f747c072e54f
jin <2441566215@qq.com>
2024-04-05 01:31:13 +0100

    1

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -96,6 +96,7 @@ const viewTemplate = (
   modal
   >
   <div>
+    
   <WebcamCapture id={props.id} photoedTask={props.photoedTask} 
   imgSrc={imgSrc} setImgSrc={setImgSrc}/>
   </div>
@@ -132,86 +133,83 @@ return <li className="todo">{isEditing ? editingTemplate : viewTemplate}</li>;
 
   }
 
-const WebcamCapture = ({ imgSrc, setImgSrc, ...props }) => {
+  const WebcamCapture = ({ imgSrc, setImgSrc, ...props }) => {
     const webcamRef = useRef(null);
-    
+
     const [imgId, setImgId] = useState(null);
     const [photoSave, setPhotoSave] = useState(false);
-  
+
     useEffect(() => {
-    if (photoSave) {
-    console.log("useEffect detected photoSave");
-    props.photoedTask(imgId);
-    setPhotoSave(false);
-    }
-    });
+        if (photoSave) {
+            console.log("useEffect detected photoSave");
+            props.photoedTask(imgId);
+            setPhotoSave(false);
+        }
+    }, [photoSave, imgId, props]);
+
     console.log("WebCamCapture", props.id);
-   
-  
-    const capture = useCallback( (id) => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    setImgSrc(imageSrc);
-    console.log("capture", imageSrc.length, id);
-    },
-    [webcamRef, setImgSrc]
-    );
- 
-    const savePhoto = (id, imgSrc) => {
-    console.log("savePhoto", imgSrc.length, id);
-    addPhoto(id, imgSrc);
-    setImgId(id);
-    setPhotoSave(true);
-    setReturnToMain(true);
-    };
-    const [returnToMain, setReturnToMain] = useState(false); 
 
-    const cancelPhoto = (id, imgSrc) => {
-      if (!imgSrc) {
-       alert("img is empty");
-        return;
-      }else{
-        alert("cancelPhoto", imgSrc.length, id);
-        setImgSrc(null);
-      }
-      
-   
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current.getScreenshot();
+        setImgSrc(imageSrc);
+        console.log("capture", imageSrc.length, props.id);
+    }, [webcamRef, setImgSrc, props.id]);
 
-      
-    };
+    const savePhoto = useCallback(() => {
+        console.log("savePhoto", imgSrc.length, props.id);
+        addPhoto(props.id, imgSrc);
+        setImgId(props.id);
+        setPhotoSave(true);
+    }, [props.id, imgSrc]);
+
+    const cancelPhoto = useCallback(() => {
+        if (!imgSrc) {
+            alert("img is empty");
+        } else {
+            alert(`cancelPhoto ${imgSrc.length}, ${props.id}`);
+            setImgSrc(null);
+        }
+    }, [imgSrc, props.id, setImgSrc]);
+
     return (
-    <>
-    {!imgSrc && (
-    <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
-    )}
-    {imgSrc && <img src={imgSrc} />}
-    <div className="btn-group">
-    {!imgSrc && ( 
-    <button
-    type="button"
-    className="btn"
-    onClick={() => capture(props.id)}>
-    Capture photo
-    </button>
-    )}
-    {imgSrc && ( 
-    <button
-    type="button"
-    className="btn"
-    onClick={() => savePhoto(props.id, imgSrc)}>
-    Save Photo
-    </button>
-    )}
-    <button 
-    type="button"
-    className="btn todo-cancel"
-    onClick={() => cancelPhoto(props.id, imgSrc)
-    
-    }>
-    Cancel
-    </button>
-    </div>
-    </>);
-   };
+        <>
+            {!imgSrc && (
+                <Webcam 
+                    audio={false} 
+                    ref={webcamRef} 
+                    screenshotFormat="image/jpeg" 
+                    style={{ maxWidth: '100%', height: 'auto' }} // 控制大小
+                />
+            )}
+            {imgSrc && <img src={imgSrc} alt="Captured" />}
+            <div className="btn-group">
+                {!imgSrc && (
+                    <button
+                        type="button"
+                        className="btn"
+                        onClick={capture}>
+                        Capture photo
+                    </button>
+                )}
+                {imgSrc && (
+                    <button
+                        type="button"
+                        className="btn"
+                        onClick={savePhoto}>
+                        Save Photo
+                    </button>
+                )}
+                <button 
+                    type="button"
+                    className="btn todo-cancel"
+                    onClick={cancelPhoto}>
+                    Cancel
+                </button>
+            </div>
+        </>
+    );
+};
+
 
 
    const ViewPhoto = (props) => {
